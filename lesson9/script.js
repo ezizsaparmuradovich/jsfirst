698238560d01ce6a2a8ff71b5309f847a2c47dc8
Eziz <eziz_kurbannazarow@mail.ru>
2019-03-03 11:27:24 +0300

    komit7

---
+++ lesson9/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let money, time;
+    money = prompt("Ваш бюджет на месяц? ", "");
+    time = prompt("Введите дату в формате YYYY-MM-DD ", "");
+
+let appDate = {
+    budget: money,
+    timedate: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце ", ""),
+    a2 = prompt("Во сколько обойдется? ", ""),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце ", ""),
+    a4 = prompt("Во сколько обойдется? ", "");
+
+appDate.expenses.a1 = a2;
+appDate.expenses.a1 = a2;
+
