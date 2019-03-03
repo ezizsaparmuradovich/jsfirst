27930c5edd0f77cdaf8dfa0214a29c3f9c8ecc9a
Eziz <eziz_kurbannazarow@mail.ru>
2019-03-03 09:59:18 +0300

    this is third comit

--- lesson7/main.js
+++ lesson7/main.js
@@ -1,16 +1,24 @@
 'use strict';
 
 let money, time;
-    money = prompt("Ваш бюджет на месяц? ");
-    time = prompt("Введите дату в формате YYYY-MM-DD ");
-let m = prompt("Введите обязательную статью расходов в этом месяце ");
-let t = prompt("Во сколько обойдется? ");
+    money = prompt("Ваш бюджет на месяц? ", "");
+    time = prompt("Введите дату в формате YYYY-MM-DD ", "");
+
 let appDate = {
-    mony: money,
+    budget: money,
     timedate: time,
-    expenses: {m: t},
+    expenses: {},
     optionalExpenses: {},
     income: [],
     savings: false
 };
-alert(appDate.mony/30)
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце ", ""),
+    a2 = prompt("Во сколько обойдется? ", ""),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце ", ""),
+    a4 = prompt("Во сколько обойдется? ", "");
+
+appDate.expenses.a1 = a2;
+appDate.expenses.a1 = a2;
+
+alert(appDate.budget/30)
