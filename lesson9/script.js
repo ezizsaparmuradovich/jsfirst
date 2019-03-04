e97efba70094e57bc8c0e766217c210c4686b13f
Eziz <eziz_kurbannazarow@mail.ru>
2019-03-04 09:26:01 +0300

    Комит для 2 задание. 4/03/2019

--- lesson9/script.js
+++ lesson9/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let money, time;
-    money = prompt("Ваш бюджет на месяц? ", "");
+    money = +prompt("Ваш бюджет на месяц? ", "");
     time = prompt("Введите дату в формате YYYY-MM-DD ", "");
 
 let appDate = {
@@ -13,11 +13,49 @@ let appDate = {
     savings: false
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце ", ""),
-    a2 = prompt("Во сколько обойдется? ", ""),
-    a3 = prompt("Введите обязательную статью расходов в этом месяце ", ""),
-    a4 = prompt("Во сколько обойдется? ", "");
+for(let i=0; i<2; i++){
+    let a = prompt("Введите обязательную статью расходов в этом месяце ", ""),
+        b = prompt("Во сколько обойдется? ", "");
+    if(typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length<50){
+        appDate.expenses[a] = b;
+        console.log("done");
+    }else{
+        i=0;
+    }
+}
+// let i=0;
+// while(i<2){
+//     let a = prompt("Введите обязательную статью расходов в этом месяце ", ""),
+//         b = prompt("Во сколько обойдется? ", "");
+//     if(typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length<50){
+//         appDate.expenses[a] = b;
+//         console.log("done");
+//     }else{
+//         i=0;
+//     }
+//     i++;
+// }
+// var i=0;
+// do {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце ", ""),
+//         b = prompt("Во сколько обойдется? ", "");
+//     if(typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length<50){
+//         appDate.expenses[a] = b;
+//         console.log("done");
+//     }else{
+//         i=0;
+//     }
+//     i++;
+// } while (i<2);
+appDate.moneyOneDay = appDate.budget/30;
+console.log(appDate.moneyOneDay);
 
-appDate.expenses.a1 = a2;
-appDate.expenses.a1 = a2;
-
+if(appDate.moneyOneDay < 100){
+    console.log("Мнимальный уровень достатка!");
+}else if(appDate.moneyOneDay>100 && appDate.moneyOneDay<2000){
+    console.log("Средный уровень достатка!");
+}else if(appDate.moneyOneDay>2000){
+    console.log("Высокий уровень достатка!");
+}else{
+    console.log("Призашла какая-то ошибочка!");
+}
